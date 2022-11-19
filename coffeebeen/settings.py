d2262dc04f888061e106ab407db24f3a01daafe3
simonpojok <simonpojok@gmail.com>
2022-11-19 10:42:14 +0300

    added allowed hosts for deployment test

--- coffeebeen/settings.py
+++ coffeebeen/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rylxggv&6kob(a4v72cetj8(j@v=$0wxguldb=jnwtkixuzdex
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['coffeebeen.pythonanywhere.com']
 
 
 # Application definition
